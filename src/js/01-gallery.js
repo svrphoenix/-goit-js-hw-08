c5974b5db3cdaa46b2433bbd92430cd8d32c57f1
Serhii Rudenko <svr.phoenix@gmail.com>
2023-02-22 22:29:09 +0100

    Adding Task 1, Task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,27 @@ import { galleryItems } from './gallery-items';
 // Change code below this linen
 
 console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryRef = document.querySelector('.gallery');
+const galleryItemsMarkup = galleryItems
+  .map(
+    ({preview, original, description}) =>
+      `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`
+  )
+  .join('');
+galleryRef.innerHTML = galleryItemsMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const setSavedCurrentTime = function(savedCurrentTime, playerInstance) {
+  playerInstance.setCurrentTime(savedCurrentTime).then(function (seconds) {
+  console.log('Playing from saved time: ', seconds)
+  }).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log('Range Error');
+        break;
+      default:
+        console.log('Error: ', error.name);
+        break;
+    }
+  })
+}
+
+const onTimeUpdate = function (currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+}
+
+setSavedCurrentTime(localStorage.getItem('videoplayer-current-time'), player);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
